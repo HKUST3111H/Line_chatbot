1543d5a3c002852d79f17e2516d4b5474c1b5bd9
zhmeishi <zhmeishi@gmail.com>
2017-09-18 10:14:09 +0800

    Test 2

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
@@ -36,7 +36,7 @@ public class DatabaseEngine {
 			
 			while (result == null && (sCurrentLine = br.readLine()) != null) {
 				String[] parts = sCurrentLine.split(":");
-				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
+				if (text.toLowerCase().contains(parts[0].toLowerCase())) {
 					result = parts[1];
 				}
 			}
--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
@@ -74,4 +74,16 @@ public class KitchenSinkTester {
 		assertThat(!thrown);
 		assertThat(result.equals("def"));
 	}
+	@Test
+	public void testFound_1() throws Exception {
+		boolean thrown = false;
+		String result = null;
+		try {
+			result = this.databaseEngine.search("hahaabc");
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown);
+		assertThat(result.equals("def"));
+	}
 }
