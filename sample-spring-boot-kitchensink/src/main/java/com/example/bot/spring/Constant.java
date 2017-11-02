21ed44d14350b7ee39e3a3b08216474203bbb901
gwangaj <gwangaj@connect.ust.hk>
2017-11-03 01:09:39 +0800

    try to push

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Constant.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Constant.java
@@ -1,5 +1,18 @@
 package com.example.bot.spring;
+import lombok.extern.slf4j.Slf4j;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
 
+@Slf4j
 public  class Constant {
 	
 	// FAQ
